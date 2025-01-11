b5dcab0cecc908d5c35c5113eb93d0e157bc9868
TunmiDev <adewoleoluwatunmise@gmail.com>
2025-01-11 14:00:14 +0100

    added articles

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Choose from "./Components/Choose";
+import Articles from "./Components/Article";
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
       <Choose />
+      <Articles />
     </div>
   );
 };
